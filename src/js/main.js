b35d641af76ecde8187603be653df02ed90633ad
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-05-02 17:05:14 -0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,7 @@
-const form = document.getElementById('form-financeiro');
-
-const saida = document.getElementById('saida');
-const entrada = document.getElementById('entrada');
-
-function validateForm(saida, entrada) {
-    const saidaNum = parseInt(saida);
-    const entradaNum = parseInt(entrada);
-    if(saidaNum > entradaNum){
-        return false;
-    } else {
-        return true;
-    }
-}
-
-form.addEventListener('submit', function(e) {
+$('form').on('submit', function(e) {
     e.preventDefault();
-
-    const messageSuccess = "Finanças do mês cadastradas com sucesso!";
-    const messageFailure = "A saída não pode ser maior que a entrada!";
-
-    const validate = validateForm(saida.value, entrada.value);
-
-    if(validate){
-        const successContainer = document.querySelector('.success-message');
-        successContainer.innerHTML = messageSuccess;
-        successContainer.style.display = 'block';
-    } else {
-        saida.style.border = '2px solid red';
-        const failureContainer = document.querySelector('.failure-message');
-        failureContainer.innerHTML = messageFailure;
-        failureContainer.style.display = 'block';
-    }
-})
-
-saida.addEventListener('change', function() {
-    validateChange = validateForm(saida.value, entrada.value);
-
-    if(validateChange){
-        saida.style.border = '';
-        document.querySelector('.failure-message').style.display = 'none';
-    } else {
-        document.querySelector('.success-message').style.display = 'none';
-    }
-})
-
-entrada.addEventListener('change', function() {
-    validateChange = validateForm(saida.value, entrada.value);
-
-    if(validateChange){
-        saida.style.border = '';
-        document.querySelector('.failure-message').style.display = 'none';
-    } else {
-        document.querySelector('.success-message').style.display = 'none';
-    }
+    const addTarefaValue = $('#adiciona-tarefa').val();
+    const novoItem = $('<li></li>');
+    $(`<a id="${addTarefaValue}">${addTarefaValue}</a>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
 })
